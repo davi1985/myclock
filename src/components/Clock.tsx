e395a332ecb51f4e7a35bb3d3ae2f7b77f308684
Davi Silva <davisilvaphoto@gmail.com>
2022-03-31 07:03:07 -0300

    fix property beforeOrAfterMidday

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -6,27 +6,28 @@ export const Clock = ({
   minute,
   second,
   beforeOrAfterMidday,
-}: TimeProps) => {
-  return (
-    <div className="clock-container">
-      <div className="clock-col">
-        <p className="clock-hours clock-timer">
-          {String(hour).padStart(2, "0")}
-        </p>
-        <p className="clock-label">Hours</p>
-      </div>
-      <div className="clock-col">
-        <p className="clock-minutes clock-timer">{minute}</p>
-        <p className="clock-label">Minutes</p>
-      </div>
-      <div className="clock-col">
-        <p className="clock-seconds clock-timer">{second}</p>
-        <p className="clock-label">Seconds</p>
-      </div>
+}: TimeProps) => (
+  <div className="clock-container">
+    <div className="clock-col">
+      <p className="clock-hours clock-timer">{String(hour).padStart(2, "0")}</p>
+      <p className="clock-label">Hours</p>
+    </div>
+
+    <div className="clock-col">
+      <p className="clock-minutes clock-timer">{minute}</p>
+      <p className="clock-label">Minutes</p>
+    </div>
+
+    <div className="clock-col">
+      <p className="clock-seconds clock-timer">{second}</p>
+      <p className="clock-label">Seconds</p>
+    </div>
+
+    {beforeOrAfterMidday && (
       <div className="clock-col">
         <p className="clock-day clock-timer">{beforeOrAfterMidday}</p>
         <p className="clock-label">AM/PM</p>
       </div>
-    </div>
-  );
-};
+    )}
+  </div>
+);
